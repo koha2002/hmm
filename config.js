8ea1d55a67a09cdb3461b89acab20aaed7a39cdc
koha2002 <85944261+koha2002@users.noreply.github.com>
2021-06-15 20:43:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Đông anh à!',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao m cứ kiêu vl
-    Đã seen lại còn đíu rep
-    Hầy Đông Anh là thế seo,
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Cho t vay tiền học lại nhó nhó :> 😙',
-    desc: 'Nghèo lắm rồi đang không đủ tiền ăn cơm :( ',
-    btnYes: 'oki lun :3 :3',
-    btnNo: 'ĐÉO :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao m lại đông ý cho t vay tiền :>',
-    btnReply: 'Gửi cho mày',
-    reply: 'cho vay thì cho vay mà không cho vay thì cho vay',
-    mess: 'Tao bít mò :> . Cảm mơn nhó :)))',
-    messDesc: 'Đmm tối nay đi xin code đi mm mai thi rồi',
-    btnAccept: 'Okiiiii lun ',
-    messLink: 'http://fb.com/koha2002' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
